bc25848232d26c833011f29bdc0797d1d883f078
leonsit2345 <76213025+leonsit2345@users.noreply.github.com>
2024-11-04 22:27:50 -0400

    Add files via upload

---
+++ Tlakualtia.js
@@ -0,0 +1,33 @@
+function mostrar(){
+    document.getElementById('fotitos').style.display = 'flex';
+    document.getElementById('boton').style.display = 'none';
+    document.getElementById('colegio').style.display = 'none';
+    document.getElementById('redes').style.display = 'none';
+    document.getElementById('boton2').style.display = 'none';
+    document.getElementById('botones').style.display = 'none';
+    document.getElementById('vuelta').style.display = 'block';
+}
+function ocultar(){
+    document.getElementById('fotitos').style.display = 'none';
+    document.getElementById('boton').style.display = 'block';
+    document.getElementById('colegio').style.display = 'block';
+    document.getElementById('redes').style.display = 'block';
+    document.getElementById('boton2').style.display = 'block';
+    document.getElementById('botones').style.display = 'block';
+    document.getElementById('vuelta').style.display = 'none';
+}
+function mostrar2(){
+    document.getElementById('formulario').style.display = 'block';
+    document.getElementById('fotitos').style.display = 'none';
+    document.getElementById('boton').style.display = 'none';
+    document.getElementById('boton2').style.display = 'none';
+    document.getElementById('colegio').style.display = 'none';
+    document.getElementById('redes').style.display = 'none';
+}
+function ocultar2(){
+    document.getElementById('formulario').style.display = 'none';
+    document.getElementById('boton2').style.display = 'block';
+    document.getElementById('boton').style.display = 'block';
+    document.getElementById('colegio').style.display = 'block';
+    document.getElementById('redes').style.display = 'block';
+}
